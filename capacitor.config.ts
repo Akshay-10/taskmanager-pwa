40c9c85594c6ad799d1f3a32156f420a0ff7ac5b
Akshay <akshay@thealteroffice.com>
2025-01-13 20:26:07 +0530

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.akshay.taskmanager',
+  appName: 'TaskManager',
+  webDir: 'build'
+};
+
+export default config;
